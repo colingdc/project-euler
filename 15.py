2c40e785af335c97bd9e85e659241c08f40cc58a
Colin Grodecoeur <clngrdcr@gmail.com>
2018-01-30 20:59:01 +0100

    Problem 15

---
+++ 15.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+# How many such routes are there through a 20×20 grid?
+
+from math import factorial
+import sys
+
+n = int(sys.argv[1])
+print factorial(2 * n) / factorial(n) ** 2
